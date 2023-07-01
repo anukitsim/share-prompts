707039ae1efcfc9077b23ad4b988c9f64ffae8d8
anukitsim <104069419+anukitsim@users.noreply.github.com>
2023-07-01 13:54:03 +0400

    basic setup of the project and file structure, installed dcrypt next-auth mongodb and mongoose, added tailwind config file and global css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,11 +10,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        notoSansJP: ["Noto Sans JP", "sans-serif"],
       },
+      colors: {
+        'primary-gray': '#BDCDD6',
+      }
     },
   },
   plugins: [],
